0fdbb0cce6afbc985c4e856994d869b7e419d9d2
MedJensen <medalm980622@student.jenseneducation.se>
2025-09-20 17:28:50 +0200

    Added message when the form is submitted

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.querySelector(".menu-toggle").addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("show");
 });
+
+// Contact form validation
+const form = document.getElementById("contactForm");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+    const feedback = document.getElementById("formMessage");
+
+    feedback.textContent = "Thank you!";
+      feedback.style.color = "green";
+      form.reset();
+
+  });
